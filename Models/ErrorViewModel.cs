2d18cb07246bd428c961b130227672ff224d4035
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2026-04-05 20:30:12 +0200

    Bootstrap project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SupplyChainLab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
